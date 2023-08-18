76533f41ebca5801c224e852f5ac872e645ffd76
Your Name <stevehuko@gmail.com>
2023-08-18 20:34:36 +0300

    initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include<stdbool.h>
+#include <stdio.h>
+
+int main (void)
+{
+    int i=5;
+    while(i>0){
+        printf("Meow\n");
+        i--;
+    }
+    for(int f=7;f>0;f--){
+        printf("Woof!\n");
+    }
+    while(true){
+        printf("Moooo\n");
+    }
+}
